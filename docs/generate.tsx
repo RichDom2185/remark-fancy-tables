65b3c44d94b48d4cdd63522a328a00c65d47cc11
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2025-05-31 23:57:33 +0800

    Simplify docs generation script

--- docs/generate.tsx
+++ docs/generate.tsx
@@ -1,5 +1,3 @@
-import { watch } from "node:fs";
-import path from "node:path";
 import { Docs } from "./src";
 import { writeToFile } from "./src/utils";
 
@@ -12,17 +10,3 @@ const generate = async () => {
 };
 
 generate();
-if (process.env.NODE_ENV === "development") {
-  const dirname = import.meta.dirname;
-  const filesSrc = path.join(dirname, "./src");
-  const docsSrc = path.join(dirname, "./docs/src");
-  console.log(`Watching for changes in ${filesSrc} and ${docsSrc}`);
-
-  const watcher1 = watch(filesSrc, { recursive: true }, generate);
-  const watcher2 = watch(docsSrc, { recursive: true }, generate);
-  process.on("SIGINT", () => {
-    watcher1.close();
-    watcher2.close();
-    process.exit(0);
-  });
-}
