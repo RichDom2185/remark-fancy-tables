9fe8861753cd0d949d77dc5564fb2a49a733f972
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2025-05-31 22:01:21 +0800

    Refactor to separate handler file

---
+++ src/handler.ts
@@ -0,0 +1,17 @@
+import { defaultHandlers } from "mdast-util-to-hast";
+
+/**
+ * Default code block handler.
+ */
+const defaultHandler = defaultHandlers.code;
+type Handler = typeof defaultHandler;
+
+export const fancyTables: Handler = (s, node) => {
+  if (node.lang !== "table") {
+    return defaultHandler(s, node);
+  }
+  // TODO: Implement fancy tables plugin
+  const contents = node.value;
+  console.log(contents);
+  return defaultHandler(s, node);
+};
