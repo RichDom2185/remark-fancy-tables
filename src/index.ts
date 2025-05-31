48943af63c60100b03038f1fd7c2e0e2f34ba673
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2025-05-31 21:06:03 +0800

    Scaffold basic plugin structure

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello World!");
+import { defaultHandlers } from "mdast-util-to-hast";
+
+/**
+ * Default code block handler.
+ */
+const defaultHandler = defaultHandlers.code;
+type Handler = typeof defaultHandler;
+
+const fancyTables: Handler = (s, node) => {
+  if (node.lang !== "table") {
+    return defaultHandler(s, node);
+  }
+  // TODO: Implement fancy tables plugin
+  const contents = node.value;
+  console.log(contents);
+  return defaultHandler(s, node);
+};
+
+export default fancyTables;
